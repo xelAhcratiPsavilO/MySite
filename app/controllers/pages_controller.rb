1a60db063f4578e7a6b115e7c05ec6dee07c6d8a
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2019-12-12 20:36:22 +0000

    Create views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,20 @@ class PagesController < ApplicationController
   def home
 
   end
+
+  def accommodations
+
+  end
+
+  def afterparty
+
+  end
+
+  def rsvp
+
+  end
+
+  def video
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome' => 'pages#home'
+  root 'pages#home'
+  get 'accommodations' => 'pages#accommodations'
+  get 'afterparty' => 'pages#afterparty'
+  get 'registry' => 'pages#registry'
+  get 'rsvp' => 'pages#rsvp'
+  get 'video' => 'pages#video'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
