6b48ac56c1f188c906a44336b04eff9673be5234
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2019-12-14 15:44:08 +0000

    Created basic form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,10 +13,28 @@ class PagesController < ApplicationController
   end
 
   def rsvp
-    @rsvps = Rsvp.all 
+    @rsvp = Rsvp.new
+  end
+
+  def create
+    @rsvp = Rsvp.new(rsvp_params)
+    if @rsvp.save
+      redirect_to action: 'rsvp_index'
+    else
+      render 'rsvp'
+    end
+  end
+
+  def rsvp_index
+    @rsvps = Rsvp.all
   end
 
   def video
 
+  end
+
+  private
+  def rsvp_params
+    params.require(:rsvp).permit(:content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'afterparty' => 'pages#afterparty'
   get 'registry' => 'pages#registry'
   get 'rsvp' => 'pages#rsvp'
+  post 'rsvp' => 'pages#create'
+  get 'rsvp/index' => 'pages#rsvp_index'
   get 'video' => 'pages#video'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
