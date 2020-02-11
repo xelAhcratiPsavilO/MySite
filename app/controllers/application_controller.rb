77920e79ba62a04fa3b69dd15a957ef3d207991c
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2020-02-11 23:59:32 +0000

    Fix typo in locale declaration by removing blank space

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  # before_action :set_locale
-  #
-  # private
-  #
-  # def set_locale
-  #   I18n.locale = params[:locale] if params[:locale].present?
-  # end
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] if params[:locale].present?
+  end
 
 end
--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,1 +1,8 @@
-Rails.application.config.i18n.default_locale = :es
+# Where the I18n library should search for translation files
+I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+
+# Permitted locales available for the application
+I18n.available_locales = [:en, :es]
+
+# Set default locale to something other than :en
+I18n.default_locale = :es
