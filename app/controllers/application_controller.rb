23b2b7713342653558f97f531b1f059027ee7ec2
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2020-02-12 00:15:02 +0000

    Edited url for endpoints and root path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
     I18n.locale = params[:locale] if params[:locale].present?
   end
 
+  def default_url_options(options = {})
+    {locale: I18n.locale}  
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
-  root 'pages#home'
-  get 'accommodation' => 'pages#accommodation'
-  get 'afterparty' => 'pages#afterparty'
-  post 'afterparty' => 'pages#create_afterparty'
-  get 'afterparty/index' => 'pages#afterparty_index'
-  get 'registry' => 'pages#registry'
-  get 'rsvp' => 'pages#rsvp'
-  post 'rsvp' => 'pages#create'
-  get 'rsvp/index' => 'pages#rsvp_index'
-  get 'video' => 'pages#video'
+  scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
+    root 'pages#home'
+    get 'accommodation' => 'pages#accommodation'
+    get 'afterparty' => 'pages#afterparty'
+    post 'afterparty' => 'pages#create_afterparty'
+    get 'afterparty/index' => 'pages#afterparty_index'
+    get 'registry' => 'pages#registry'
+    get 'rsvp' => 'pages#rsvp'
+    post 'rsvp' => 'pages#create'
+    get 'rsvp/index' => 'pages#rsvp_index'
+    get 'video' => 'pages#video'
+  end
+  match '*path', to: redirect("/#{I18n.default_locale}/%{path}"), :via => [:get, :post]
+  match '', to: redirect("/#{I18n.default_locale}"), :via => [:get, :post]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
